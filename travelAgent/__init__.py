31d9a385e145f62a0361c293df8564c2ccbff1a8
Yi Cheng <yi.cheng@ucdconnect.ie>
2023-02-25 01:12:28 +0800

    login func

---
+++ travelAgent/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+import travelAgent.config
+from travelAgent.config import Config
+from flask_sqlalchemy import SQLAlchemy
+
+# create variable app
+app = Flask(__name__)
+
+# passing all the configuration information from config.py
+app.config.from_object(Config)
+
+# create database for the app
+db = SQLAlchemy(app)
+
+
+# Very important!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# from travelAgent import app, models
--- travelAgent/app.py
+++ travelAgent/app.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template
 import logging
 
-app = Flask(__name__)
+from flask_login import LoginManager
 
+from travelAgent.models import User
+
+app = Flask(__name__)  # create an app instance
+app.secret_key = 'BxeE3wJcjYi6yA7y1bjBJ1IAs0'
+
+# -------------------------------------create a logger------------------------------------------
 logger = logging.getLogger(__name__)  # create a logger
 logger.setLevel(logging.INFO)  # show messages above info level
 fh = logging.FileHandler('travelAgent.log')  # log file handler
@@ -15,6 +21,10 @@ ch.setFormatter(formatter)
 logger.addHandler(fh)
 logger.addHandler(ch)
 
+# -------------------------------------login/logout------------------------------------------
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 
 
 @app.route('/')
@@ -22,15 +32,22 @@ def idnex():  # put application's code here
     logger.info('Entered the HOME page')
     return render_template("index.html")
 
+
 @app.route('/about')
 def about():
     logger.info('Entered the ABOUT page')
     return render_template("about.html")
 
+
 @app.route('/customer_login')
 def customer_login():
     logger.info('Entered the CUSTOMER LOGIN page')
     return render_template("customer_login.html")
+
+
+@login_manager.user_loader
+def load_user(userid):
+    return User.get(userid)
 
 
 if __name__ == '__main__':
