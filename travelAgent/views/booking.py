5e125028b9559a30ecc5d89d17c52de0fded4a70
Jiayi Xu <jiayi.xu@ucdconnect.ie>
2023-04-03 17:25:20 +0800

    delete booking and change booking details

--- travelAgent/views/booking.py
+++ travelAgent/views/booking.py
@@ -34,3 +34,40 @@ def addBooking(combination_id):
             # print("hhhhhhhhhhhh")
             return render_template('book.html', combination_id=combination_id)
 
+
+@booking_blueprint.route('/deleteBooking/<booking_id>', methods=['GET', 'POST'])
+def deleteBooking(booking_id):
+    # db.session.query(RecordC).filter(RecordC.id == booking_id).delete()
+    booking = RecordC.query.filter_by(id=booking_id).first()
+    db.session.delete(booking)
+    db.session.commit()
+    return render_template('profile.html')
+
+@booking_blueprint.route('/changeBooking/<booking_id>', methods=['GET', 'POST'])
+def changeBooking(booking_id):
+    form = BookingForm(request.form)
+    if request.method == 'GET':
+        # bookings = RecordC.query.all()
+        # print("Ssdssssssssssss")
+        return render_template('profile.html', form=form, booking_id=booking_id)
+    else:
+        if form.validate_on_submit():
+            start_time = form.time.data
+            num = form.num.data
+            name = form.name.data
+            tel = form.tel.data
+            RecordC.query.filter_by(id=booking_id).update({'start_time': start_time, 'num': num, 'name': name, 'tel': tel })
+            db.session.commit()
+            # print("ffffffffffffffffff")
+            return render_template('profile.html')
+        else:
+            # print("hhhhhhhhhhhh")
+            return render_template('profile.html')
+
+
+
+
+
+
+
+
