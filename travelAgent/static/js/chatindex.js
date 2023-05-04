f010db3d87548d988ecdc4702724b1463e4cac8e
Jiayi Xu <jiayi.xu@ucdconnect.ie>
2023-05-04 15:07:51 +0800

    chat

--- travelAgent/static/js/chatindex.js
+++ travelAgent/static/js/chatindex.js
@@ -1,3 +1,8 @@
+// from fuzzywuzzy import process
+// from fuzzywuzzy import fuzz
+//import fuzzywuzzy from './chat'
+
+
 //translate function
 function Translate(event) {
     console.log(event.target)
@@ -42,32 +47,44 @@ async function add_messages(msg, scroll) {
     console.log(msg.name)
     if (typeof msg.name !== "undefined") {
         var date = dateNow();
-
+       //  var hours = date.hours;
+       //  print(hours)
+       //  var msg1 = msg.time.split(" ")
+       //  var b = msg1[1]
+       //  var t = b.split(":")
+       //  var time1 = t[0]
+        // console.log("time = " + time)
         if (typeof msg.time !== "undefined") {
             var n = msg.time;
         } else {
             var n = date;
         }
         var username = load_username()
-
+        //staff侧人工回复
         var content =
             '<div class="row">' +
-
-
             // '<div class="col-sm">'+
             '<div class="alert alert-shadow col-5" style="margin-left: 2%; background: rgb(124,168,246, 0.4)">' +
             '<b style="color:#734" class="right">' +
             msg.name +
             '</b><p>' +
             msg.message +
-            '</p>' + `<button class="btn btn-primary" onclick=Translate(event)>Translate</button>` + '<span class="res_translate"></span>' + '<span class="time-right">' +
-            n +
+            '</p>'
+            + ////////////////
+              '<p>' +
+                 `<button class="btn btn-primary"  onclick=Translate(event)>Translate</button>` + '<span class="res_translate"></span>' + '<span class="time-right">' +
+             '</p>' +
+                 '<p style="color: grey">'+
+                  n +
+                 '</p>'+
+                 /////////////////
             '</span></div>' +
             '<div class="col"></div>' +
             '<div class="col"></div>' +
             '</div></div>';
-
+        //当前用户，但不用自动回复
         if (username == msg.name) {
+            //print(msg.time)
             content =
                 '<div class="row">' +
 
@@ -79,11 +96,274 @@ async function add_messages(msg, scroll) {
                 msg.name +
                 '</b><p style="text-align: right; color: black">' +
                 msg.message +
-                '</p>' + '<button class="btn btn-primary" onclick=Translate(event)>Translate</button>' + '<span class="res_translate"></span>' + '<span class="time-left">' +
-                n +
+                '</p>'
+               /////////////////
+                 + '<p style="text-align: right">'
+                 +'<button class="btn btn-primary"  onclick=Translate(event)>Translate</button>' + '<span class="res_translate"></span>' + '<span class="time-right">' +
+               '</p>'
+             + '<p style="text-align: right; color: grey" >'
+                 +n+
+                 '</p>' +
+                 /////////////////
                 '</span></div>' +
                 '</div></div>';
+
+            //每次用户发信息，都
         }
+
+        //用户出发关键词，staff侧自动回复
+        //'When staff work'
+        if (username == msg.name && 'When staff work?' == msg.message) {
+        // if (username == msg.name && fuzz.ratio('When staff work', msg.message)>20){
+             var content =
+             '<div class="row">' +
+
+                '<div class="col"></div>' +
+                '<div class="col"></div>' +
+                // '<div class="col-sm">'+
+                '<div class="alert alert-success alert-shadow col-5" style="margin-right: 2%; background: rgba(67,227,203,0.4)">' +
+                '<b style="color:#157" class="left">' +
+                msg.name +
+                '</b><p style="text-align: right; color: black">' +
+                msg.message +
+                '</p>'
+                 /////////////////
+                 + '<p style="text-align: right">'
+                 +'<button class="btn btn-primary"  onclick=Translate(event)>Translate</button>' + '<span class="res_translate"></span>' + '<span class="time-right">' +
+               '</p>'
+             + '<p style="text-align: right; color: grey" >'
+                 +n+
+                 '</p>' +
+                 /////////////////
+                '</span></div>' +
+                '</div></div>' +
+
+            '<div class="row">' +
+            // '<div class="col-sm">'+
+            '<div class="alert alert-shadow col-5" style="margin-left: 2%; background: rgb(124,168,246, 0.4)">' +
+            '<b style="color:#734" class="right">' +
+            'Auto Reply' +
+            '</b><p>' +
+            'Staff work between 9am-10pm' +
+            '</p>' +
+                 ////////////////
+              '<p>' +
+                 `<button class="btn btn-primary"  onclick=Translate(event)>Translate</button>` + '<span class="res_translate"></span>' + '<span class="time-right">' +
+             '</p>' +
+                 '<p style="color: grey">'+
+                  n +
+                 '</p>'+
+                 /////////////////
+            '</span></div>' +
+            '<div class="col"></div>' +
+            '<div class="col"></div>' +
+            '</div></div>';
+        }
+
+        //link order page
+        //'How to check order status'
+         if (username == msg.name && 'How to check order status?' == msg.message) {
+             var content =
+             '<div class="row">' +
+
+                '<div class="col"></div>' +
+                '<div class="col"></div>' +
+                // '<div class="col-sm">'+
+                '<div class="alert alert-success alert-shadow col-5" style="margin-right: 2%; background: rgba(67,227,203,0.4)">' +
+                '<b style="color:#157" class="left">' +
+                msg.name +
+                '</b><p style="text-align: right; color: black">' +
+                msg.message +
+                '</p>'
+              /////////////////
+                 + '<p style="text-align: right">'
+                 +'<button class="btn btn-primary"  onclick=Translate(event)>Translate</button>' + '<span class="res_translate"></span>' + '<span class="time-right">' +
+               '</p>'
+             + '<p style="text-align: right; color: grey" >'
+                 +n+
+                 '</p>' +
+                 /////////////////
+                '</span></div>' +
+                '</div></div>' +
+
+            '<div class="row">' +
+            // '<div class="col-sm">'+
+            '<div class="alert alert-shadow col-5" style="margin-left: 2%; background: rgb(124,168,246, 0.4)">' +
+            '<b style="color:#734" class="right">' +
+            'Auto Reply' +
+            '</b><p>' +
+            'You can check order status inside ' +
+                 '<a href="http://127.0.0.1:5000/order_list">ORDERS</a>'+
+                 ' page' +
+            '</p>' +
+           ////////////////
+              '<p>' +
+                 `<button class="btn btn-primary"  onclick=Translate(event)>Translate</button>` + '<span class="res_translate"></span>' + '<span class="time-right">' +
+             '</p>' +
+                 '<p style="color: grey">'+
+                  n +
+                 '</p>'+
+                 /////////////////
+            '</span></div>' +
+            '<div class="col"></div>' +
+            '<div class="col"></div>' +
+            '</div></div>';
+        }
+
+         //超出时间 staff have go home
+          if (username == msg.name && (((n.split(' '))[1]).split(':')[0] < 9  ) && 'When staff work?' != msg.message && 'How to check order status?' != msg.message && 'Where to check my Favorites?' != msg.message &&  'How to contact Digital Beans for further cooperation?' != msg.message) {
+             var content =
+             '<div class="row">' +
+
+                '<div class="col"></div>' +
+                '<div class="col"></div>' +
+                // '<div class="col-sm">'+
+                '<div class="alert alert-success alert-shadow col-5" style="margin-right: 2%; background: rgba(67,227,203,0.4)">' +
+                '<b style="color:#157" class="left">' +
+                msg.name +
+                '</b><p style="text-align: right; color: black">' +
+                msg.message +
+                '</p>'
+                 /////////////////
+                 + '<p style="text-align: right">'
+                 +'<button class="btn btn-primary"  onclick=Translate(event)>Translate</button>' + '<span class="res_translate"></span>' + '<span class="time-right">' +
+               '</p>'
+             + '<p style="text-align: right; color: grey" >'
+                 +n+
+                 '</p>' +
+                 /////////////////
+                '</span></div>' +
+                '</div></div>' +
+
+            '<div class="row">' +
+            // '<div class="col-sm">'+
+            '<div class="alert alert-shadow col-5" style="margin-left: 2%; background: rgb(124,168,246, 0.4)">' +
+            '<b style="color:#734" class="right">' +
+            'Auto Reply' +
+            '</b><p>' +
+            'Staff work during 9am-10pm' +
+            '</p>'+ '<p>'+
+                 'Our staff will contact you ASAP later'
+                 + '</p>'+ '<p>'+
+                 'Auto Reply is available for particular quations:' +
+
+            '</p>' +
+                 ////////////////
+              '<p>' +
+                 `<button class="btn btn-primary"  onclick=Translate(event)>Translate</button>` + '<span class="res_translate"></span>' + '<span class="time-right">' +
+             '</p>' +
+                 '<p style="color: grey">'+
+                  n +
+                 '</p>'+
+                 /////////////////
+            '</span></div>' +
+            '<div class="col"></div>' +
+            '<div class="col"></div>' +
+            '</div></div>';
+        }
+
+          //link Favorites
+          //'Where to check my Favorites'
+         if (username == msg.name && 'Where to check my Favorites?' == msg.message) {
+             var content =
+             '<div class="row">' +
+
+                '<div class="col"></div>' +
+                '<div class="col"></div>' +
+                // '<div class="col-sm">'+
+                '<div class="alert alert-success alert-shadow col-5" style="margin-right: 2%; background: rgba(67,227,203,0.4)">' +
+                '<b style="color:#157" class="left">' +
+                msg.name +
+                '</b>' + '<p style="text-align: right; color: black">' +
+                msg.message +
+                '</p>'
+                 /////////////////
+                 + '<p style="text-align: right">'
+                 +'<button class="btn btn-primary"  onclick=Translate(event)>Translate</button>' + '<span class="res_translate"></span>' + '<span class="time-right">' +
+               '</p>'
+             + '<p style="text-align: right; color: grey" >'
+                 +n+
+                 '</p>' +
+                 /////////////////
+                '</span></div>' +
+                '</div></div>' +
+
+            '<div class="row">' +
+            // '<div class="col-sm">'+
+            '<div class="alert alert-shadow col-5" style="margin-left: 2%; background: rgb(124,168,246, 0.4)">' +
+            '<b style="color:#734" class="right">' +
+            'Auto Reply' +
+            '</b><p>' +
+                 'You can check your Favorites inside '+
+                 '<a href="http://127.0.0.1:5000/favourites">FAVORITES</a>'+
+                 ' page' +
+             //  '<button onclink="mylink(this)" value="http://127.0.0.1:5000/favourites">Favoritesss</button>'
+            '</p>' +
+                 ////////////////
+              '<p>' +
+                 `<button class="btn btn-primary"  onclick=Translate(event)>Translate</button>` + '<span class="res_translate"></span>' + '<span class="time-right">' +
+             '</p>' +
+                 '<p style="color: grey">'+
+                  n +
+                 '</p>'+
+                 /////////////////
+            '</span></div>' +
+            '<div class="col"></div>' +
+            '<div class="col"></div>' +
+            '</div></div>';
+        }
+
+          //link Contact us
+          //'Where to contact with Digital Benas'
+         if (username == msg.name && 'How to contact Digital Beans for further cooperation?' == msg.message) {
+             var content =
+             '<div class="row">' +
+
+                '<div class="col"></div>' +
+                '<div class="col"></div>' +
+                // '<div class="col-sm">'+
+                '<div class="alert alert-success alert-shadow col-5" style="margin-right: 2%; background: rgba(67,227,203,0.4)">' +
+                '<b style="color:#157" class="left">' +
+                msg.name +
+                '</b><p style="text-align: right; color: black">' +
+                msg.message +
+                '</p>'
+                 /////////////
+                  + '<p style="text-align: right">'
+                 +'<button class="btn btn-primary"  onclick=Translate(event)>Translate</button>' + '<span class="res_translate"></span>' + '<span class="time-right">' +
+               '</p>'
+             + '<p style="text-align: right; color: grey" >'
+                 + n +
+                 '</p>' +
+                 /////////////
+                '</span></div>' +
+                '</div></div>' +
+
+            '<div class="row">' +
+            // '<div class="col-sm">'+
+            '<div class="alert alert-shadow col-5" style="margin-left: 2%; background: rgb(124,168,246, 0.4)">' +
+            '<b style="color:#734" class="right">' +
+            'Auto Reply' +
+            '</b><p>' +
+                 'You can contact with Digital Beans for further cooperation inside '+
+                 '<a href="http://127.0.0.1:5000/contactUs">CONTACT US</a>'+
+                 ' page' +
+             //  '<button onclink="mylink(this)" value="http://127.0.0.1:5000/favourites">Favoritesss</button>'
+            '</p>' +
+                 ////////////////
+              '<p>' +
+                 `<button class="btn btn-primary"  onclick=Translate(event)>Translate</button>` + '<span class="res_translate"></span>' + '<span class="time-right">' +
+             '</p>' +
+                 '<p style="color: grey">'+
+                  n +
+                 '</p>'+
+                 /////////////////
+            '</span></div>' +
+            '<div class="col"></div>' +
+            '<div class="col"></div>' +
+            '</div></div>';
+        }
+
         // update div
         var messageDiv = document.getElementById("messages");
         messageDiv.innerHTML += content;
@@ -92,6 +372,10 @@ async function add_messages(msg, scroll) {
     if (scroll) {
         scrollSmoothToBottom("messages");
     }
+}
+function mylink(link) {
+    window.location.href="http://127.0.0.1:5000/favourites";
+    console.log(link.value);
 }
 
 function load_username() {
